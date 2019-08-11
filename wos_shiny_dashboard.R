1f17c29b455053a09675d43fb372707ecf6c9f86
Josh Quan <wrathofquan@gmail.com>
2019-08-11 13:16:51 -0700

    dashboard

---
+++ wos_shiny_dashboard.R
@@ -0,0 +1,48 @@
+library(RPostgres)
+library(RPostgreSQL)
+library(odbc)
+library(dbplyr)
+library(tidyverse)
+library(shiny)
+library(DBI)
+library(shinydashboard)
+
+
+ui <- dashboardPage(skin = 'black',
+  dashboardHeader(title = "Web of Science Query Tool", titleWidth = 300),
+  dashboardSidebar(
+    textInput("query",label = "Enter SQL Query:", placeholder = "SELECT * FROM wos_address_organizations, wos_summary_names WHERE organization LIKE 'Stanford%'", value = "SELECT * FROM wos_address_organizations, wos_summary_names WHERE organization LIKE 'Stanford%'" ),
+    numericInput("nrows", "Enter the number of rows to display:", 10),
+    downloadButton("downloadData", "Download")),
+  dashboardBody(
+    fluidRow(div(style = 'overflow-x: scroll', tableOutput("tbl")))
+    ))
+    
+
+  
+server <- function(input, output, session) {
+  data <- shiny::reactive(data.frame())
+  
+  output$tbl <- renderTable({
+    con <- dbConnect(drv = RPostgres::Postgres(),
+                     dbname = "wos",
+                     host = "localhost",
+                     user = "wos_admin",
+                     port = "5432")
+    on.exit(dbDisconnect(con), add = TRUE)
+    dbGetQuery(con, paste0(input$query, paste0("LIMIT "),input$nrows, ";"))
+  })
+  
+  # output$tbl<-renderTable(table())
+  # output$connectionlist <- eventReactive(input$list,{dbGetQuery(con, "show processlist")})
+  # 
+  ## try to download as csv
+  output$downloadData <- downloadHandler(
+    filename = function() { paste(input$query,".csv",sep = "") },
+    content = function(file) {
+      write.csv(output$tbl, file)
+      
+    })
+}
+
+shinyApp(ui, server)
