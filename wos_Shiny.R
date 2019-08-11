4fb2acd5339f69b30c1d86b3e5cf7e6077f7ed3e
Josh Quan <wrathofquan@gmail.com>
2019-08-10 21:14:20 -0700

    query functionality

--- wos_Shiny.R
+++ wos_Shiny.R
@@ -6,27 +6,26 @@ library(tidyverse)
 library(shiny)
 library(DBI)
 
-
-
 ui <- fluidPage(
   headerPanel("Web of Science WebUI"),
+  textInput("query",label = "Enter SQL Query Here:", placeholder = "SELECT * FROM wos_summary;", value = "SELECT * FROM wos_summary LIMIT "),
   numericInput("nrows", "Enter the number of rows to display:", 5),
-  tableOutput("tbl"),
-  downloadButton("downloadData", "Download")
+  downloadButton("downloadData", "Download"),
+  tableOutput("tbl")
   
 )
 
 server <- function(input, output, session) {
   output$tbl <- renderTable({
     con <- dbConnect(drv = RPostgres::Postgres(),
-                dbname = "wos",
-                host = "localhost",
-                user = "wos_admin",
-                port = "5432")
+                     dbname = "wos",
+                     host = "localhost",
+                     user = "wos_admin",
+                     port = "5432")
     on.exit(dbDisconnect(con), add = TRUE)
-    dbGetQuery(con, paste0(
-      "SELECT id, title FROM wos_titles LIMIT ", input$nrows, ";"))
+    dbGetQuery(con, paste0(input$query,  input$nrows, ";"))
   })
+  
 }
 
 shinyApp(ui, server)
