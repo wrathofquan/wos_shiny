e289e9372a95b4795992916d300d77a09edda63c
Josh Quan <wrathofquan@gmail.com>
2019-08-11 17:43:29 -0700

    textAreaInput for query box

--- wos_shiny_dashboard_export.R
+++ wos_shiny_dashboard_export.R
@@ -10,7 +10,7 @@ library(shinydashboard)
 ui <- dashboardPage(skin = 'black',
                     dashboardHeader(title = "Web of Science Query Tool", titleWidth = 300),
                     dashboardSidebar(
-                      textInput("query",label = "Enter SQL Query:", placeholder = "SELECT * FROM wos_address_organizations, wos_summary_names WHERE organization LIKE 'Stanford%'", value = "SELECT * FROM wos_address_organizations, wos_summary_names WHERE organization LIKE 'Stanford%'" ),
+                      textAreaInput("query",label = "Enter SQL Query:", placeholder = "SELECT * FROM wos_address_organizations, wos_summary_names WHERE organization LIKE 'Stanford%'", value = "SELECT * FROM wos_address_organizations, wos_summary_names WHERE organization LIKE 'Stanford%'" ),
                       numericInput("nrows", "Enter the number of rows to display:", 10),
                       downloadButton("downloadData", "Download")),
                     dashboardBody(
