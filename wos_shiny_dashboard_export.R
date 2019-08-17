0dec1b9627847f03ad968086b82fa22d7f262a6a
Josh Quan <wrathofquan@gmail.com>
2019-08-17 16:40:29 -0700

    data tables

--- wos_shiny_dashboard_export.R
+++ wos_shiny_dashboard_export.R
@@ -2,19 +2,24 @@ library(RPostgres)
 library(RPostgreSQL)
 library(odbc)
 library(dbplyr)
-library(tidyverse)
 library(shiny)
 library(DBI)
 library(shinydashboard)
+library(DT)
 
 ui <- dashboardPage(skin = 'black',
-                    dashboardHeader(title = "Web of Science Query Tool", titleWidth = 300),
+                    dashboardHeader(title = "Web of Science Query Tool", titleWidth = 300,
+                                    dropdownMenu(type = "notifications",
+                                                 notificationItem(
+                                                   text = "Read me",
+                                                   icon("glasses")
+                                                 ))),
                     dashboardSidebar(
-                      textAreaInput("query",label = "Enter SQL Query:", placeholder = "SELECT * FROM wos_address_organizations, wos_summary_names WHERE organization LIKE 'Stanford%'", value = "SELECT * FROM pg_catalog.pg_tables WHERE schemaname	= 'raw_data'" ),
-                      numericInput("nrows", "Enter the number of rows to display:", 100),
+                      textAreaInput("query",label = "Enter SQL Query:", placeholder = "SELECT * FROM wos_address_organizations, wos_summary_names WHERE organization LIKE 'Stanford%'", value = "SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname	= 'public' " ),
+                      numericInput("nrows", "Enter the number of rows to display:", min = 1, 20),
                       downloadButton("downloadData", "Download")),
                     dashboardBody(
-                      fluidRow(div(style = 'overflow-x: scroll', tableOutput("tbl")))
+                      fluidRow(div(style = 'overflow-x: scroll', dataTableOutput("tbl")))
                     ))
 
 
@@ -23,14 +28,15 @@ server <- function(input, output, session) {
   table <- eventReactive(list(input$query, input$nrows),{
     conn <- dbConnect(drv = RPostgres::Postgres(),
                       dbname = "wos",
-                      host = "localhost",
+                      host = "wos.cxo3wqeijrjm.us-east-2.rds.amazonaws.com",
                       user = "wos_admin",
+                      password = "ssds3141",
                       port = "5432")
     on.exit(dbDisconnect(conn), add = TRUE)
     dbGetQuery(conn, paste0(input$query, paste0("LIMIT "),input$nrows, ";"))
   })
   
-  output$tbl<-renderTable(table())
+  output$tbl<-renderDataTable({table()}) 
   
   # output$connectionlist <- eventReactive(input$list,{dbGetQuery(conn, "show processlist")})
   # 
